7a49d42d316a22084c3329f10aba69e50f896f9b
JoanStoddard <joanstoddard16@gmail.com>
2020-04-27 15:37:51 -0700

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,87 @@
+var friendsArray = [
+    {
+        name: "James Bond",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fnewsdio.com%2Fwp-content%2Fuploads%2F2019%2F12%2FThe-5-best-and-5-worst-recast-choices-ever-made.jpg&imgrefurl=https%3A%2F%2Fnewsdio.com%2F10-things-that-daniel-craigs-link-did-differently-that-the-franchise-should-maintain%2F26277%2F&tbnid=8qIcfuL602zeSM&vet=12ahUKEwj3ieu9-ojpAhXEBDQIHVkIAt8QMygcegQIARBH..i&docid=qkSSSw1ZnsiqnM&w=1710&h=900&q=james%20bond%20link&ved=2ahUKEwj3ieu9-ojpAhXEBDQIHVkIAt8QMygcegQIARBH",
+        scores: [
+            "5",
+            "5",
+            "4",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "4",
+            "5"
+        ]
+    },
+
+    {
+        name: "M",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fassets.mi6-hq.com%2Fimages%2Fstills%2Fqos_061_280.jpg&imgrefurl=https%3A%2F%2Fwww.mi6-hq.com%2Fsections%2Fallies%2Fm3&tbnid=U79VcOkgVPC6HM&vet=12ahUKEwjj_vTg-ojpAhVwATQIHRaBA64QMygCegUIARCYAg..i&docid=CybVyKWECx9xdM&w=280&h=435&q=m%20from%20james%20bond%20image&ved=2ahUKEwjj_vTg-ojpAhVwATQIHRaBA64QMygCegUIARCYAg",
+        scores: [
+            "5",
+            "3",
+            "2",
+            "5",
+            "5",
+            "4",
+            "5",
+            "5",
+            "5",
+            "4"
+        ]
+    },
+    {
+        name: "Jinx",
+        photo: "https://www.google.com/search?q=jinx+james+bond+image&tbm=isch&ved=2ahUKEwjj_vTg-ojpAhVwATQIHRaBA64Q2-cCegQIABAA&oq=jinx+james+bond+image&gs_lcp=CgNpbWcQAzoECAAQQzoCCAA6BggAEAcQHjoGCAAQBRAeULHeAliR7wJgyfkCaABwAHgCgAGGBIgBiBSSAQswLjQuMy4xLjEuMZgBAKABAaoBC2d3cy13aXotaW1n&sclient=img&ei=PP-mXuPwL_CC0PEPloKO8Ao&bih=686&biw=1396&rlz=1C1CHBF_enUS883US883",
+        scores: [
+            "5",
+            "5",
+            "4",
+            "5",
+            "5",
+            "3",
+            "4",
+            "3",
+            "3",
+            "2"
+        ]
+    },
+
+    {
+        name: "Vesper",
+        photo: "https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwiks66M-4jpAhVDo54KHeTzCLUQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.reddit.com%2Fr%2FJamesBond%2Fcomments%2Ff80k2u%2Fi_love_vesper_lynd%2F&psig=AOvVaw0XGRLXb-Ixf13eT6oaEByt&ust=1588089111350192",
+        scores: [
+            "3",
+            "3",
+            "2",
+            "4",
+            "5",
+            "1",
+            "3",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Q",
+        photo: "https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwi9guyt-4jpAhVQip4KHeFHCOIQjRx6BAgBEAQ&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FQ_(James_Bond)&psig=AOvVaw2puug02y2S2JJqEwRSaYQe&ust=1588089181556828",
+        scores: [
+            "2",
+            "2",
+            "5",
+            "4",
+            "5",
+            "1",
+            "4",
+            "5",
+            "5",
+            "1"
+        ]
+    }
+    
+]
+module.exports = friendsArray;
+
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,45 @@
+var friendList = require("../data/friends.js");
+
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendList);
+    });
+
+
+    app.post("/api/friends", function (req, res) {
+
+        var newFriend = {
+            name: "",
+            photo: "",
+            differencePoints: 200,
+
+        }
+
+        var newUserScore = req.body.scores;
+        var friendDifference;
+
+        for (var i = 0; i < friendList.length; i++) {
+            var currentFriend = friendList[i];
+            friendDifference = 0
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentScore = currentFriend.scores[j];
+                var newUserScore = newUserScore[j];
+
+                friendDifference += Math.abs(parseInt(newUserScore) - parseInt(currentScore));
+
+
+            }
+            if (friendDifference <= newFriend.differencePoints) {
+                newFriend.name = currentFriend.name
+                newFriend.photo = currentFriend.photo
+                newFriend.differencePoints = friendDifference
+            }
+        }
+
+        friendList.push(req.body);
+        res.json(newFriend);
+    })
+}
+
