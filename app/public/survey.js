f9486135a20f004e4ce121713051becb732a744f
JoanStoddard <joanstoddard16@gmail.com>
2020-04-29 15:03:41 -0700

    jquery

---
+++ app/public/survey.js
@@ -0,0 +1,10 @@
+     $(document).ready(function(){
+        $("#submitButton").on("click", function(event) {
+            event.preventDefault();
+            console.log("working");
+        })
+
+     });
+
+     
+        
